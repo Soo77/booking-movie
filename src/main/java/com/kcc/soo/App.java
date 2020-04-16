a5518e0d0f849d2715621b3eed03ad69f2da20ef
Soo77 <shimsh3@naver.com>
2020-04-16 14:17:53 +0900

    gradle init again

--- src/main/java/com/kcc/soo/App.java
+++ src/main/java/com/kcc/soo/App.java
@@ -5,8 +5,7 @@ package com.kcc.soo;
 
 public class App {
     public String getGreeting() {
-        return "Hello world. Hihi";
-       
+        return "Hello world.";
     }
 
     public static void main(String[] args) {
